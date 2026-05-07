f703f01adbea138d468cda10651aec9e3a87eabb
PatoKuakMO <edmosquera@unimayor.edu.co>
2026-05-07 15:29:50 -0500

    actualizacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./services/auth";
-import { Navbar } from "./components/Navbar";
+import { Navbar } from "./components/navbar";
 import { Lobby } from "./components/lobby";
 import { Home } from "./components/home";
-import { Profile } from "./components/Profile";
+import { Profile } from "./components/profile";
 import { Gallery } from "./components/gallery";
 import './App.css'
 
